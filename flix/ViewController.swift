503e7cfefd2106e678d277ad7a2fc35eb440a6c9
徐婷 <xuting@nilai.local>
2019-02-21 16:47:49 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by 徐婷 on 2019/2/21.
+//  Copyright © 2019年 manjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
