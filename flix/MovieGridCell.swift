d1dd2599223baaa7448a7960f39bfaf162aedb93
徐婷 <xuting@nilai.local>
2019-02-28 20:46:22 -0500

    Finished part 2 of Flix

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by 徐婷 on 2019/2/28.
+//  Copyright © 2019年 manjin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
